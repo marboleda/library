e5d9451cc683cfae5199cc938c30fc097e3523bb
Marco Arboleda <arboleda.marco@gmail.com>
2020-05-01 19:36:59 -0400

    Implement button that allows user to toggle read status

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,10 @@ function Book(title, author, numPages, readStatus) {
     this.author = author;
     this.numPages = numPages;
     this.readStatus = readStatus;
+    
+    Book.prototype.toggleReadStatus = function() {
+        this.readStatus = (this.readStatus == "read") ? "unread" : "read";
+    }
 }
 
 function addBookToLibrary(book){
@@ -32,6 +36,7 @@ function render(library) {
     const bookNumPages = document.createElement("p");
     const bookReadStatus = document.createElement("p");
     const deleteBookButton = document.createElement("button");
+    const toggleReadStatusButton = document.createElement("button");
     const indexTracker = document.createAttribute("data-booknum");
 
     bookCard.classList.add("card");
@@ -40,29 +45,39 @@ function render(library) {
         bookTitle.textContent = book.title;
         bookAuthor.textContent = book.author;
         bookNumPages.textContent = book.numPages;
+
         bookReadStatus.textContent = book.readStatus;
+        bookReadStatus.classList.add("read-status");
 
         indexTracker.value = myLibrary.length + index;
-
         deleteBookButton.textContent = `Delete Book ${indexTracker.value}`;
-        deleteBookButton.classList.add("deleteBookButton");
+        deleteBookButton.classList.add("delete-book");
+
+        toggleReadStatusButton.textContent = "Toggle Read\\Unread";
+        toggleReadStatusButton.classList.add("toggle-read-status");
 
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(bookNumPages);
         bookCard.appendChild(bookReadStatus);
         bookCard.setAttributeNode(indexTracker);
-
         bookCard.appendChild(deleteBookButton);
+        bookCard.appendChild(toggleReadStatusButton);
 
         body.appendChild(bookCard.cloneNode(true));      
         bookCard.textContent = ""; 
     });
 
-    document.querySelectorAll(".deleteBookButton").forEach(b => b.onclick = function() {
+    document.querySelectorAll(".delete-book").forEach(b => b.onclick = function() {
         let bookIndex = b.parentElement.getAttribute("data-booknum");
         myLibrary.splice(bookIndex,1);
         document.querySelector(`[data-booknum='${bookIndex}'`).remove();
+    });
+
+    document.querySelectorAll(".toggle-read-status").forEach(b => b.onclick = function() {
+        let bookIndex = b.parentElement.getAttribute("data-booknum");
+        myLibrary[bookIndex].toggleReadStatus();
+        b.parentElement.querySelector(".read-status").textContent = myLibrary[bookIndex].readStatus;
     });
 
 }
